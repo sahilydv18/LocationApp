7422e92085be311c8da602f0125cfb853a7e8ea1
Sahil Yadav <raosahil15266@gmail.com>
2024-04-17 14:36:59 +0530

    added documentation

--- app/src/main/java/com/example/locationapp/LocationUtils.kt
+++ app/src/main/java/com/example/locationapp/LocationUtils.kt
@@ -6,10 +6,8 @@ import android.content.pm.PackageManager
 import androidx.core.content.ContextCompat
 
 class LocationUtils(context: Context) {
-    fun hasLocationPermission(context: Context): Boolean {      // This checks is the user has given location permission or not
-        return ContextCompat.checkSelfPermission(
-            context,
-            Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED
+    fun hasLocationPermission(context: Context): Boolean {      // This checks if the user has given location permission or not
+        return ContextCompat.checkSelfPermission(context, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED
                 &&
                 ContextCompat.checkSelfPermission(context,Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED
     }
--- app/src/main/java/com/example/locationapp/MainActivity.kt
+++ app/src/main/java/com/example/locationapp/MainActivity.kt
@@ -41,7 +41,7 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun MyApp() {
-    val context = LocalContext.current
+    val context = LocalContext.current  // Context of the current screen we are a
     val locationUtils = LocationUtils(context)
     LocationDisplay(locationUtils = locationUtils, context = context)
 }
@@ -50,11 +50,16 @@ fun MyApp() {
 @Composable
 fun LocationDisplay(locationUtils: LocationUtils, context: Context) {
 
+    // This requestPermissionLauncher gives us UI for asking permission to the user and we can either request single permission or multiple permissions
     val requestPermissionLauncher = rememberLauncherForActivityResult(contract = ActivityResultContracts.RequestMultiplePermissions()) {
         permissions ->
         if (permissions[Manifest.permission.ACCESS_FINE_LOCATION] == true && permissions[Manifest.permission.ACCESS_COARSE_LOCATION] == true) {
-            // we have permission
+            // When we ask for permission and the user gives us the permission then we write the code that will be executed in that case here
         } else {
+            // If user declines our permission we then give them reason for taking permission to run app using rationale
+            // rationale helps us to ask again for permission if we are denied permission for the first time but it is only limited to one time
+            // means we can only ask again for permission only once after getting rejected for first time
+            // Rationale basically helps us to ask for permission a second time if we are denied for the first time
             val rationaleRequired = ActivityCompat.shouldShowRequestPermissionRationale(
                 context as MainActivity,
                 Manifest.permission.ACCESS_FINE_LOCATION
@@ -64,8 +69,10 @@ fun LocationDisplay(locationUtils: LocationUtils, context: Context) {
             )
 
             if(rationaleRequired) {
+                // If user declines permission for first time show the user this toast and now we can ask for permission one more time
                 Toast.makeText(context,"Location Permission required to run app", Toast.LENGTH_LONG).show()
             } else {
+                // If user declines permission for the second time then show this toast and now we can't ask for permission again, user needs to go to settings to turn on permission
                 Toast.makeText(context,"Turn on location from setting", Toast.LENGTH_LONG).show()
             }
         }
@@ -81,6 +88,8 @@ fun LocationDisplay(locationUtils: LocationUtils, context: Context) {
             if (locationUtils.hasLocationPermission(context)) {
                 // We have location
             } else {
+                // Here we start the launcher for the first time, as we start the app for first time we don't have any permissions so we are asking for permission
+                // via requestPermissionLauncher for location(fine and coarse)
                 requestPermissionLauncher.launch(
                     arrayOf(
                         Manifest.permission.ACCESS_FINE_LOCATION,
